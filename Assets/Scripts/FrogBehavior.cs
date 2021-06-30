c0a72b14155fa324fb4e40cbc28f95359c414f7f
jeniferirwin <jenifer.irwin@gmail.com>
2021-06-29 23:40:34 -0400

    Frogs now jump at diff rates with proper animation

--- Assets/Scripts/FrogBehavior.cs
+++ Assets/Scripts/FrogBehavior.cs
@@ -14,7 +14,7 @@ public class FrogBehavior : MonoBehaviour
         anim = GetComponent<Animator>();
         anim.SetTrigger("Idle");
         moveSpeed = Random.Range(1,8);
-        jumpCooldown = 1;
+        jumpCooldown = Random.Range(2f, 5.5f);
         if (Random.Range(1,49) <= 50)
             isJumper = true;
         else
@@ -26,7 +26,7 @@ public class FrogBehavior : MonoBehaviour
         jumpCooldown -= Time.fixedDeltaTime;
         if (isJumper && jumpCooldown <= 0)
         {
-            jumpCooldown = Random.Range(1, 3f);
+            jumpCooldown = Random.Range(1, 5f);
             Jump();
         }
         else if (!isJumper)
@@ -37,10 +37,29 @@ public class FrogBehavior : MonoBehaviour
         }
     }
     
+    /*
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Jumped");
+        }
+    }
+    */
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            //anim.ResetTrigger("Jump");
+            anim.SetTrigger("Idle");
+        }
+    }
     private void Jump()
     {
+        anim.ResetTrigger("Idle");
         anim.SetTrigger("Jump");
-        var force = transform.forward * Random.Range(100f, 175f);
+        var force = (transform.forward + transform.up) * Random.Range(50f, 100f);
         rb.AddForce(force, ForceMode.Impulse);
     }
 }
