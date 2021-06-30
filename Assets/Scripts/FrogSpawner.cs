229cd19cf61ff973b571d199e6aa19c491d45ca7
jeniferirwin <jenifer.irwin@gmail.com>
2021-06-30 00:30:43 -0400

    Different types of frogs now spawn

--- Assets/Scripts/FrogSpawner.cs
+++ Assets/Scripts/FrogSpawner.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class FrogSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject frogPrefab;
+    private float cooldown;
+
+    private void Awake()
     {
-        
+        cooldown = GetCooldown();
+    }
+    
+    private float GetCooldown()
+    {
+        return Random.Range(5f,15f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cooldown -= Time.deltaTime;
+
+        if (cooldown <= 0)
+        {
+            cooldown = GetCooldown();
+            ReleaseFrog();
+        }
+    }
+    
+    void ReleaseFrog()
+    {
+        var newFrog = GameObject.Instantiate(frogPrefab);
+//        newFrog.transform.Rotate(new Vector3(0,0,0));
+        var frogController = newFrog.GetComponent<FrogController>();
+        switch (Random.Range(1,6))
+        {
+            case 1:
+              frogController.Blue();
+              break;
+            case 2:
+              frogController.BalckOnRedSpot();
+              break;
+            case 3:
+              frogController.OrangeBlackBlue();
+              break;
+            case 4:
+              frogController.RedGreenBlack();
+              break;
+            case 5:
+              frogController.Yellow();
+              break;
+            default:
+              frogController.YellowOnBlack();
+              break;
+        }
     }
 }
